1bc31936c2684050dd9fe3242106b55671300f23
ganeswar-sharma <ms23btech11034@iith.ac.in>
2026-08-31 16:49:37 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+   public:
+    vector<vector<int>> buildAdj(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adjList(n);
+        for (int i = 0; i < edges.size(); i++) {
+            vector<int> edge = edges[i];
+            int src = edge[0];
+            int dest = edge[1];
+            adjList[dest].push_back(src);
+        }
+        return adjList;
+    }
+
+    void dfs(vector<vector<int>>& adjList, vector<bool>& vis, int node, int parent, bool& isCycle,
+             vector<bool>& path) {
+        vis[node] = true;
+        path[node] = true;
+        for (int i = 0; i < adjList[node].size(); i++) {
+            int neigh = adjList[node][i];
+            if (!vis[neigh]) {
+                dfs(adjList, vis, neigh, node, isCycle, path);
+            } else if (vis[neigh] && path[neigh] == true) {
+                isCycle = true;
+                return;
+            }
+        }
+        path[node] = false;
+        return;
+    }
+
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adjList = buildAdj(n, edges);
+        bool isCycle = false;
+        vector<bool> vis(n, false);
+        vector<bool> path(n, false);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(adjList, vis, i, -1, isCycle, path);
+
+                if (isCycle) return false;
+            }
+        }
+        return !isCycle;
+    }
+};
